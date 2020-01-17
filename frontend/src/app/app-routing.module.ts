a0ecc21fc069788214978edbcf5b372e6470ab63
Jennifer Cicvarova <jennifer_cicvarova@sluz.ch>
2020-01-17 11:02:28 +0100

    added entities, service and routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { BasketComponent } from './components/basket/basket.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: 'details', component: ProductDetailsComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'basket', component: BasketComponent },
+  { path: '', redirectTo: '/overview', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/entities/basketItem.ts
@@ -0,0 +1,9 @@
+import { Product } from './product';
+
+export class BasketItem {
+
+    product: Product;
+    quantity: number;
+    total: number;
+
+}
